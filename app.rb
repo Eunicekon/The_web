8f57fadc7a24e4cfb5f5c53b34cd07af36f3856d
Eunice Kontor <Eunice.Kontor@ft.com>
2020-10-27 08:06:45 +0000

    created new cat-form route &  added post method on the form and named-cat route

--- app.rb
+++ app.rb
@@ -9,17 +9,21 @@ get '/secret' do #second route
   'Did someone say Monday!...'
 end
 
-get '/Eunice' do
-  'Shotgun, do you even work?'
+get '/Eunice' do #third route just printing the string inside it
+  'Shotgun, do you even work?' 
 end
 
 get '/random-cat' do
-  @name = ["Amigo", "Oscar", "Viking"].sample
+  @name = ["Amigo", "Oscar", "Viking"].sample #this will sample the name so everytime you refresh the page you get a different name
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do #we've changed the route method to post beacsue we want it to be able tp handle the post request from the cat_form.erb file 
   p params
   @name = params[:name]
   erb(:index)
+end
+
+get '/cat-form' do 
+  erb :cat_form
 end
